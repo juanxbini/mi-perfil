090a5466da44a61e3c2327c937e6a7438af47323
juanxbini <juanxbini@gmail.com>
2025-07-18 19:43:56 -0300

    projects update

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,18 +3,25 @@
 
 export const projects = [
   {
-    titulo: "Sistema de Control para Panadería",
-    url: "https://github.com/tuusuario/panaderia-control",
-    tags: ["React", "Node.js", "MongoDB"]
-  },
-  {
     titulo: "Sistema de Monitoreo Indoor con Arduino",
-    url: "https://github.com/tuusuario/indoor-iot",
+    url: "https://github.com/juanxbini/indoor-system",
     tags: ["Arduino", "IoT", "JavaScript"]
   },
   {
     titulo: "Landing Page de Portfolio Personal",
-    url: "https://github.com/tuusuario/landing-portfolio",
+    url: "https://github.com/juanxbini/mi-perfil",
     tags: ["HTML", "CSS", "JavaScript"]
-  }
+  },
+  {
+  titulo: "Sistema de Autenticación con React + Redux Toolkit",
+  url: "https://github.com/juanxbini/authentication-redux",
+  tags: ["React", "Redux Toolkit", "Thunk", "Autenticación", "API REST"]
+},
+{
+  titulo: "API REST de Autenticación – Node + Express + MongoDB",
+  url: "https://github.com/juanxbini/auth-rest-node",
+  tags: ["Node.js", "Express", "MongoDB", "JWT", "API REST"]
+}
+
+
 ];
